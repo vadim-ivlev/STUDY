ee19454aae88acace3f6e31648a874aef263ba55
vadim-ivlev <vadim.ivlev@gmail.com>
2017-09-06 05:04:08 +0300

    es

--- prolog/learnprolognow/ex2.pl
+++ prolog/learnprolognow/ex2.pl
@@ -21,7 +21,7 @@ word(determiner,a).
          word(noun,Word5).
 
 
-%word(astante,  a,s,t,a,n,t,e). 
+% word(astante,  a,s,t,a,n,t,e). 
 %word(astoria,  a,s,t,o,r,i,a). 
 %word(baratto,  b,a,r,a,t,t,o). 
 %word(cobalto,  c,o,b,a,l,t,o). 
