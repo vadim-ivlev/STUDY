0fd76ee683115c785c9a9905b665a9184abfd222
vadim-ivlev <vadim.ivlev@gmail.com>
2018-02-04 17:07:44 +0300

    .

---
+++ ES6/a.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CSProject0
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
